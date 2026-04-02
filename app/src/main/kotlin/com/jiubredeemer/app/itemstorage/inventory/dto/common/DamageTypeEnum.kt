54dec5cc29ea683648971944315727295793e9c1
jiubredeemer <jiubredeemer@gmail.com>
2026-04-02 14:38:47 +0400

    PRO-0: add damage type

--- app/src/main/kotlin/com/jiubredeemer/app/itemstorage/inventory/dto/common/DamageTypeEnum.kt
+++ app/src/main/kotlin/com/jiubredeemer/app/itemstorage/inventory/dto/common/DamageTypeEnum.kt
@@ -4,5 +4,15 @@ enum class DamageTypeEnum(val apiName: String) {
     CRUSHING("Дробящий"),
     STABBING("Колющий"),
     CHOPPING("Рубящий"),
+    ACID("Кислотный"),
+    COLD("Холодом"),
+    FIRE("Огненный"),
+    FORCE("Силовой"),
+    LIGHTNING("Молнией"),
+    NECROTIC("Некротический"),
+    POISON("Ядовитый"),
+    PSYCHIC("Психический"),
+    RADIANT("Сияющий"),
+    THUNDER("Громовой"),
     NO("")
 }
